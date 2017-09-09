a88d28d37ed9594f19f681b0e811e3e80bc1cfac
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-09-09 14:38:46 +0900

    Edit model/user.rb
    Change “null: :false” into “null: false”

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, null: :false
+  validates :username, null: false
 end
