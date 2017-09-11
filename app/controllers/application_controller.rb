60d1867f462f353a15b0fbd4172b14246d46876f
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-09-11 15:06:35 +0900

    Change into way_of_prefix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_out_path_for(resource)
-    '/users/sign_in'
+    new_user_session_path
   end
 
   def configure_permitted_parameters
