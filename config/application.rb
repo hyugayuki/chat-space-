c1d3e9162430238ce006d14b5f879de04880e285
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-09-11 16:36:06 +0900

    Edit to read  "devise.en.yml"

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.helper false
       g.javascripts false
