ec98fe2aa65acdabeea5b54a0b9d8ac68b3a6bee
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-09-13 17:27:01 +0900

    define the associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  has_many :members, dependent: :destroy
+  has_many :groups, through: "members"
   validates :username, null: false
 end
