d43d9a7f497bc1e8f7f3748caa9711356a3d4227
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-09-13 17:36:20 +0900

    Define varidates for {groups: :name}

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,3 +2,7 @@ class Group < ApplicationRecord
   has_many :members, dependent: :destroy
   has_many :users, through: "members"
   accepts_nested_attributes_for :members, allow_destroy: true
+
+  validates :name, null: false
+  validate :add_error_sample
+
