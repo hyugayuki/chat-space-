06095aaf50d26f3f78e1d78b96f7d4dce8d3c0a6
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-09-13 17:37:05 +0900

    Edit to display error messages

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,3 +6,10 @@ class Group < ApplicationRecord
   validates :name, null: false
   validate :add_error_sample
 
+  def add_error_sample
+    # nameが空のときにエラーメッセージを追加する
+    if name.empty?
+      errors.add(:name, "を入力してください")
+    end
+  end
+end
