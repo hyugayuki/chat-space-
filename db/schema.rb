cf02e3bb85d12dcd86eabd73dd6badc0865383d0
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-09-15 20:39:26 +0900

    Define messages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912093508) do
+ActiveRecord::Schema.define(version: 20170915113425) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 20170912093508) do
     t.index ["group_id"], name: "index_members_on_group_id", using: :btree
     t.index ["user_id", "group_id"], name: "index_members_on_user_id_and_group_id", using: :btree
     t.index ["user_id"], name: "index_members_on_user_id", using: :btree
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.string   "image"
+    t.integer  "user_id",    null: false
+    t.integer  "group_id",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["content", "image", "user_id", "group_id"], name: "index_messages_on_content_and_image_and_user_id_and_group_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
