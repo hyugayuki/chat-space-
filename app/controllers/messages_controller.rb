9f5ecf7b211ca488915365e5cdd27535798ee716
hyugayuki <hyugayuki@hyuugayuukinoMacBook-Pro.local>
2017-09-16 13:17:21 +0900

    Define routes and nethods

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,21 @@
 class MessagesController < ApplicationController
 
   def index
+    @message = Message.new
+    @group = Group.find(params[:group_id])
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 end
